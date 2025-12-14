678fdd065c0db2728df3a89359336842fbd7357b
wecand0 <user@wecand0.local>
2025-12-14 18:36:21 +0300

    use clang-format off, cause pch must be the first.

--- benchmark/astar_benchmark.cpp
+++ benchmark/astar_benchmark.cpp
@@ -1,14 +1,20 @@
 #include <benchmark/benchmark.h>
 
-#include <astar.h>
+// clang-format off
 #include <pch.h>
+#include "astar.h"
+// clang-format on
 
 static void BM_StarPath(benchmark::State &state) {
     H3AStar astar;
     const H3Index start = 0x822a87fffffffffL;
     const H3Index end = 0x8eb8a6b13046757L;
+    std::vector<H3Index> path;
     for (const auto &_ : state) {
-        auto path = astar.findShortestPath(start, end);
+        path = astar.findShortestPath(start, end);
+        for (const auto &__ : path) {
+        }
+        path.clear();
     }
 }
 BENCHMARK(BM_StarPath);
